003105adbaf5a1bf4266b288df2b165963741731
Andre Gomes <andre_colossus@yahoo.com.br>
2016-09-08 14:53:30 -0300

    outro

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -110,9 +110,11 @@ MEDIA_URL = '/media/'
 #Email
 #EMAIL_BACKEND = 'django.core.mail.backends.stmp.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'Nome <email@gmail.com>'
+DEFAULT_FROM_EMAIL = 'Nome <andrecolossus@gmail.com>'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'email@gmail.com'
 EMAIL_HOST_PASSWORD = 'senha'
 EMAIL_PORT = 587
+
+CONTACT_EMAIL = 'cgomes.andre@yahoo.com'
